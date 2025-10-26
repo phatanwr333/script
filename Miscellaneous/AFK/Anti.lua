3705a6fb3c053a9cdc9000455afa6b4eed1f73a3
phatanwr333 <123556098+phatanwr333@users.noreply.github.com>
2025-10-26 18:01:17 +0700

    Create Anti.lua

---
+++ Miscellaneous/AFK/Anti.lua
@@ -0,0 +1,17 @@
+assert(firesignal, "Your exploit does not support firesignal.")
+
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local VirtualUser = game:GetService("VirtualUser")
+
+UserInputService.WindowFocusReleased:Connect(function()
+    RunService.Stepped:Wait()
+    if firesignal then
+        pcall(firesignal, UserInputService.WindowFocused)
+    end
+end)
+
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    VirtualUser:CaptureController()
+    VirtualUser:ClickButton2(Vector2.new())
+end)
