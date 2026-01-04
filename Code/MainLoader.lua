ab030b3b72f85080826e2677948f9c07298afb01
phatanwr333 <123556098+phatanwr333@users.noreply.github.com>
2026-01-04 23:53:09 +0700

    Update MainLoader.lua

--- Code/MainLoader.lua
+++ Code/MainLoader.lua
@@ -1,1 +1,20 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/4dfe5763b0c0c46fcadb29cdd8ebdc771e340dd8ebb75478f3c10d5695207ecc/download"))()
+local HttpService = game:GetService("HttpService")
+local ScriptUrl = "https://api.junkie-development.de/api/v1/luascripts/public/4dfe5763b0c0c46fcadb29cdd8ebdc771e340dd8ebb75478f3c10d5695207ecc/download"
+
+function Execute(url)
+    local Success, Response = pcall(function()
+        return game:HttpGet(url)
+    end)
+
+    if Success and Response then
+        local LoadSuccess, err = pcall(function()
+            loadstring(Response)()
+        end)
+
+        if not LoadSuccess then
+            warn("Failed to execute script, reason: " .. tostring(err))
+        end
+    end
+end
+
+Execute(ScriptUrl)
