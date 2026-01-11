78fe3698cd42188db0d0d33d08f803eb47ddb3eb
phatanwr333 <123556098+phatanwr333@users.noreply.github.com>
2026-01-11 22:57:56 +0700

    Update MainLoader.lua

--- Code/MainLoader.lua
+++ Code/MainLoader.lua
@@ -1,4 +1,3 @@
-local HttpService = game:GetService("HttpService")
 local ScriptUrl = "https://api.junkie-development.de/api/v1/luascripts/public/4dfe5763b0c0c46fcadb29cdd8ebdc771e340dd8ebb75478f3c10d5695207ecc/download"
 
 if getgenv().SCRIPT_KEY == nil then getgenv().SCRIPT_KEY = "" end
