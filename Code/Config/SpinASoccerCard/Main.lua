a8dbce597fdb9e9273fc58835956bee41794e4e9
phatanwr333 <123556098+phatanwr333@users.noreply.github.com>
2026-03-15 20:11:14 +0700

    Update Main.lua

--- Code/Config/SpinASoccerCard/Main.lua
+++ Code/Config/SpinASoccerCard/Main.lua
@@ -1,29 +1,29 @@
 _G.Config = {
-        ["Home"] = {},
-        ["Main"] = {
-            ["Money"] = {
-                ["Autofully Claim All Money"] = false
-            },
-            ["Wheel"] = {
-                ["Autofully Claim Wheel"] = false
-            }
+    ["Home"] = {},
+    ["Main"] = {
+        ["Money"] = {
+            ["Autofully Claim All Money"] = false
         },
-        ["Shop"] = {
-            ["Buy Pack"] = {
-                ["Selected Pack To Buy"] = nil,
-                ["Autofully Buy Selected Pack"] = false,
-                ["Delay To BuyPack"] = 1
-            },
-            ["Use Pack"] = {
-                ["Selected Pack To Use"] = nil,
-                ["Autofully Use Selected Pack"] = false,
-                ["Delay To UsePack"] = 1
-            },
-            ["Trophy"] = {
-                ["Selected Trophy To Buy"] = nil,
-                ["Autofully Buy Selected Trophy"] = false,
-                ["Delay To BuyTrophy"] = 1
-            }
+        ["Wheel"] = {
+            ["Autofully Claim Wheel"] = false
+        }
+    },
+    ["Shop"] = {
+        ["Buy Pack"] = {
+            ["Selected Pack To Buy"] = nil,
+            ["Autofully Buy Selected Pack"] = false,
+            ["Delay To BuyPack"] = 1
         },
-        ["Miscellaneous"] = {}
-    }
+        ["Use Pack"] = {
+            ["Selected Pack To Use"] = nil,
+            ["Autofully Use Selected Pack"] = false,
+            ["Delay To UsePack"] = 1
+        },
+        ["Trophy"] = {
+            ["Selected Trophy To Buy"] = nil,
+            ["Autofully Buy Selected Trophy"] = false,
+            ["Delay To BuyTrophy"] = 1
+        }
+    },
+    ["Miscellaneous"] = {}
+}
