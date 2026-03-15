9e6a80def7786fccf291be39ec8885be5ed9a754
phatanwr333 <123556098+phatanwr333@users.noreply.github.com>
2026-03-15 20:00:22 +0700

    Add configuration for SpinASoccerCard

---
+++ Code/Config/SpinASoccerCard/Main.lua
@@ -0,0 +1,29 @@
+_G.Config = {
+        ["Home"] = {},
+        ["Main"] = {
+            ["Money"] = {
+                ["Autofully Claim All Money"] = false
+            },
+            ["Wheel"] = {
+                ["Autofully Claim Wheel"] = false
+            }
+        },
+        ["Shop"] = {
+            ["Buy Pack"] = {
+                ["Selected Pack To Buy"] = nil,
+                ["Autofully Buy Selected Pack"] = false,
+                ["Delay To BuyPack"] = 1
+            },
+            ["Use Pack"] = {
+                ["Selected Pack To Use"] = nil,
+                ["Autofully Use Selected Pack"] = false,
+                ["Delay To UsePack"] = 1
+            },
+            ["Trophy"] = {
+                ["Selected Trophy To Buy"] = nil,
+                ["Autofully Buy Selected Trophy"] = false,
+                ["Delay To BuyTrophy"] = 1
+            }
+        },
+        ["Miscellaneous"] = {}
+    }
