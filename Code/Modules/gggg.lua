5b52ab18d75110116237ba0de6c9ad242ec8b189
phatanwr333 <123556098+phatanwr333@users.noreply.github.com>
2026-03-16 19:21:56 +0700

    Update gggg.lua

--- Code/Modules/gggg.lua
+++ Code/Modules/gggg.lua
@@ -6,26 +6,42 @@ local ReplicatedFirst = cloneref(game:GetService("ReplicatedFirst"))
 local StarterGui = cloneref(game:GetService("StarterGui"))
 local StarterPack = cloneref(game:GetService("StarterPack"))
 local StarterPlayer = cloneref(game:GetService("StarterPlayer"))
+local Teams = cloneref(game:GetService("Teams"))
 
 local RunService = cloneref(game:GetService("RunService"))
 local TweenService = cloneref(game:GetService("TweenService"))
 local Debris = cloneref(game:GetService("Debris"))
 local CollectionService = cloneref(game:GetService("CollectionService"))
+local PhysicsService = cloneref(game:GetService("PhysicsService"))
 
 local UserInputService = cloneref(game:GetService("UserInputService"))
 local ContextActionService = cloneref(game:GetService("ContextActionService"))
-local VirtualInputManager = cloneref(game:GetService("VirtualInputManager"))
-local VirtualUser = cloneref(game:GetService("VirtualUser"))
-
 local GuiService = cloneref(game:GetService("GuiService"))
 local CoreGui = cloneref(game:GetService("CoreGui"))
 
 local TeleportService = cloneref(game:GetService("TeleportService"))
 local MarketplaceService = cloneref(game:GetService("MarketplaceService"))
+local BadgeService = cloneref(game:GetService("BadgeService"))
+local GroupService = cloneref(game:GetService("GroupService"))
+
 local HttpService = cloneref(game:GetService("HttpService"))
 local SoundService = cloneref(game:GetService("SoundService"))
-
-local PathfindingService = cloneref(game:GetService("PathfindingService"))
 local ProximityPromptService = cloneref(game:GetService("ProximityPromptService"))
 local TextChatService = cloneref(game:GetService("TextChatService"))
+local VoiceChatService = cloneref(game:GetService("VoiceChatService"))
+
+local Chat = cloneref(game:GetService("Chat"))
+
+local PathfindingService = cloneref(game:GetService("PathfindingService"))
+local LocalizationService = cloneref(game:GetService("LocalizationService"))
+local PolicyService = cloneref(game:GetService("PolicyService"))
+local SocialService = cloneref(game:GetService("SocialService"))
+
+local ContentProvider = cloneref(game:GetService("ContentProvider"))
+
 local Stats = cloneref(game:GetService("Stats"))
+local LogService = cloneref(game:GetService("LogService"))
+
+local MaterialService = cloneref(game:GetService("MaterialService"))
+
+local RbxAnalyticsService = cloneref(game:GetService("RbxAnalyticsService"))
