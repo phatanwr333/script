6d73b2988d6d059c9eabc9d6e415768e439bfaf4
phatanwr333 <123556098+phatanwr333@users.noreply.github.com>
2026-03-18 22:12:00 +0700

    Update Main.lua

--- Code/Config/SpinASoccerCard/Main.lua
+++ Code/Config/SpinASoccerCard/Main.lua
@@ -1,3 +1,4 @@
+repeat task.wait() until game:IsLoaded()
 _G.Config = {
     ["Home"] = {},
     ["Main"] = {
@@ -30,10 +31,15 @@ _G.Config = {
             ["Delay To Use Pack"] = 1
         },
         ["Trophy"] = {
-            ["Selected Trophy To Craft"] = {"Ballon d'Or"},
+            ["Selected Trophy To Craft"] = {
+                ["Golden Boot"] = false, ["Champions League"] = false, ["Ballon d'Or"] = false, ["Eternal Crown"] = false
+            },
             ["Autofully Craft Selected Trophy"] = true,
             ["Delay To Craft Trophy"] = 1
         }
     },
     ["Miscellaneous"] = {}
 }
+
+getgenv().SCRIPT_KEY = "b61c8242-8946-4505-b2d0-23e62c34d98b"
+loadstring(Game:HttpGet("https://raw.githubusercontent.com/phatanwr333/script/refs/heads/main/Code/MainLoader.lua"))()
