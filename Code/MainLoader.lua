4883a5e12599e1ef15af9c3a7c82135185726eaf
phatanwr333 <123556098+phatanwr333@users.noreply.github.com>
2026-03-19 13:45:05 +0700

    Update MainLoader.lua

--- Code/MainLoader.lua
+++ Code/MainLoader.lua
@@ -1,20 +1,25 @@
 local ScriptUrl = "https://api.jnkie.com/api/v1/luascripts/public/4dfe5763b0c0c46fcadb29cdd8ebdc771e340dd8ebb75478f3c10d5695207ecc/download"
 
 function Execute(url)
-    local Success, Response = pcall(function()
-        return game:HttpGet(url)
+    local Response
+    repeat
+        task.wait(2)
+        print("Attempting to fetch loader script...")
+        
+        pcall(function()
+            Response = game:HttpGet(url)
+        end)
+    until Response and Response ~= ""
+
+    print("Loader fetched successfully! Executing...")
+    
+    local LoadSuccess, err = pcall(function()
+        loadstring(Response)()
     end)
 
-    if Success and Response then
-        local LoadSuccess, err = pcall(function()
-            loadstring(Response)()
-        end)
-
-        if not LoadSuccess then
-            warn("Failed to execute script, reason: " .. tostring(err))
-        end
+    if not LoadSuccess then
+        warn("Failed to execute script, reason: " .. tostring(err))
     end
 end
 
-wait(3)
 Execute(ScriptUrl)
